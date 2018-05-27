73700c55a546318d2927c02414dde7bcaa973465
prasanta <prasantakakati1994@gmail.com>
2018-05-27 17:15:57 +0530

    [TAIGA-117] wallet send user field auto complete

--- proxcdb/urls.py
+++ proxcdb/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from proxcdb.views import ProxcTransactionView
+from proxcdb import views
 
 
 urlpatterns = [
-    path('transactions/', ProxcTransactionView.as_view())
+    path('transactions/', views.ProxcTransactionView.as_view()),
+    path('users/', views.proxcdb_account_autocomplete)
 ]
--- proxcdb/views.py
+++ proxcdb/views.py
@@ -3,10 +3,11 @@ from django.contrib.auth.models import User
 from django.core.exceptions import ObjectDoesNotExist
 
 from rest_framework.views import APIView
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
-from proxcdb.models import ProxcTransaction
+from proxcdb.models import ProxcAccount, ProxcTransaction
 from proxcdb.serializers import ProxcTransactionSerializer
 
 # Create your views here.
@@ -32,19 +33,41 @@ class ProxcTransactionView(APIView):
     def post(self, request, format=None):
         try:
             to_user = User.objects.get(username=request.data.get('username'))
-            transaction = ProxcTransactionSerializer(
-                data=request.data,
-                context={'proxcaccount': request.user.proxcaccount}
-            )
-            if transaction.is_valid():
-                transaction.save(
-                    account=request.user.proxcaccount,
-                    to_account=to_user.proxcaccount
+            if to_user == request.user:
+                return Response(
+                    {'nonField': 'Can\'t send to self'},
+                    status=status.HTTP_400_BAD_REQUEST)
+            else:
+                transaction = ProxcTransactionSerializer(
+                    data=request.data,
+                    context={'proxcaccount': request.user.proxcaccount}
                 )
-                return Response(transaction.data)
-            return Response(transaction.errors,
-                            status=status.HTTP_400_BAD_REQUEST)
+                if transaction.is_valid():
+                    transaction.save(
+                        account=request.user.proxcaccount,
+                        to_account=to_user.proxcaccount
+                    )
+                    return Response(transaction.data)
+                return Response(transaction.errors,
+                                status=status.HTTP_400_BAD_REQUEST)
         except ObjectDoesNotExist:
             return Response(
                 {'nonField': 'No such user'},
                 status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view()
+@permission_classes([IsAuthenticated, ])
+def proxcdb_account_autocomplete(request):
+    data = []
+    query = request.query_params.get('username')
+    proxcaccounts = ProxcAccount.objects.filter(
+        user__username__istartswith=query
+    )
+    for proxcaccount in proxcaccounts:
+        if not proxcaccount.user == request.user:
+            data.append({
+                'label': proxcaccount.user.username,
+                'value': proxcaccount.user.username
+            })
+    return Response(data)
