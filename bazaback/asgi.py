4dd81cac0ae0ffb1d8d3e8b42fb0001835896c6f
prasanta <prasantakakati1994@gmail.com>
2018-06-04 01:22:31 +0530

    Removed sentry from asgi layer

--- bazaback/asgi.py
+++ bazaback/asgi.py
@@ -9,10 +9,4 @@ from channels.routing import get_default_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bazaback.settings")
 django.setup()
-
-SENTRY_ENABLED_IN = ['production', 'beta']
 application = get_default_application()
-
-if os.environ.get('SITE_TYPE') in SENTRY_ENABLED_IN:
-    from raven.contrib.django.raven_compat.middleware.wsgi import Sentry
-    application = Sentry(get_default_application())
