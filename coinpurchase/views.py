12c813fb9fc296703c81e779126aea7aae8bdf13
Prasanta <prasantakakati1994@gmail.com>
2018-12-21 02:28:04 +0530

    removed commented code

--- coinpurchase/views.py
+++ coinpurchase/views.py
@@ -1,14 +1,7 @@
-# from django.conf import settings
-
 from rest_framework import views
-# from rest_framework import status
 from rest_framework.response import Response
-# from rest_framework.permissions import IsAuthenticated
-
-# from oauth2_provider.contrib.rest_framework import TokenHasScope
 
 from coinpurchase.models import CoinPurchase
-# from coinpurchase.serializers import CoinPurchaseSerializer
 from coinpurchase.utils import get_coin_value
 
 
@@ -34,27 +27,3 @@ class GetTotalCoinPurchased(views.APIView):
         for coinpurchase in CoinPurchase.objects.filter(coin_name='proxcdb'):
             amount += coinpurchase.amount
         return Response({'total_purchased': amount})
-
-
-# class ProcessCoinPurchase(views.APIView):
-#     """
-#     This API will process coin purchase
-#     """
-
-#     permission_classes = (IsAuthenticated, TokenHasScope, )
-#     required_scopes = [
-#         'baza' if settings.SITE_TYPE == 'production' else 'baza-beta']
-
-#     def post(self, request, format=None):
-#         serializer = CoinPurchaseSerializer(data=request.data)
-#         if serializer.is_valid():
-#             CoinPurchase.objects.create(
-#                 user=request.user,
-#                 price=serializer.validated_data['price'],
-#                 amount=serializer.validated_data['price'] *
-#                 get_coin_value(serializer.validated_data['coin_name']),
-#                 coin_name=serializer.validated_data['coin_name']
-#             )
-#             return Response()
-#         return Response(
-#             serializer.errors, status=status.HTTP_400_BAD_REQUEST)
