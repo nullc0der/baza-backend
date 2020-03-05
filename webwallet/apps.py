feac33accd8290c18ba757ec7df0b46305f4ca3b
prasanta <prasantakakati1994@gmail.com>
2020-03-05 15:56:21 +0530

    minor change

--- webwallet/apps.py
+++ webwallet/apps.py
@@ -7,14 +7,13 @@ class WebWalletConfig(AppConfig):
     name = 'webwallet'
 
     def ready(self):
-        if os.environ.get('RUN_MAIN', None) == 'true':
-            from webwallet.api_wrapper import ApiWrapper
-            apiwrapper = ApiWrapper()
+        from webwallet.api_wrapper import ApiWrapper
+        apiwrapper = ApiWrapper()
+        res = apiwrapper.open_wallet()
+        if res.status_code == 403:
+            apiwrapper.close_wallet()
             res = apiwrapper.open_wallet()
-            if res.status_code == 403:
-                apiwrapper.close_wallet()
-                res = apiwrapper.open_wallet()
-            if res.status_code == 400 and res.json()['errorCode'] == 1:
-                res = apiwrapper.create_wallet()
-            if res.status_code == 200:
-                ApiWrapper.wallet_is_open = True
+        if res.status_code == 400 and res.json()['errorCode'] == 1:
+            res = apiwrapper.create_wallet()
+        if res.status_code == 200:
+            ApiWrapper.wallet_is_open = True
