b9cbd81f8e63bd64ac4fcbe5bbd32076a8024777
Prasanta Kakati <prasantakakati1994@gmail.com>
2020-12-04 02:30:16 +0530

    test

--- bazaback/imagesanitizer.py
+++ bazaback/imagesanitizer.py
@@ -1,5 +1,4 @@
 import os
-import logging
 import magic
 import img2pdf
 from io import BytesIO
@@ -15,8 +14,6 @@ from django.conf import settings
 from django.utils.crypto import get_random_string
 from django.core.files.base import ContentFile
 from django.core.files.storage import default_storage
-
-logger = logging.getLogger(__name__)
 
 
 def check_if_image_file(f):
@@ -48,7 +45,7 @@ def rebuild_image_and_remove_exif(image_file):
     data = list(image.getdata())
     new_image = Image.new(image.mode, image.size)
     new_image.putdata(data)
-    new_image.save(image_file, 'JPEG', quality=50)
+    new_image.save(image_file, 'JPEG', quality=80)
 
 
 def get_raw_image_name(imagedata_name):
@@ -65,27 +62,26 @@ def sanitize_image(imagedata):
     raw_image_file_path = settings.MEDIA_ROOT + '/' + default_storage.save(
         'tmp/%s-img' % get_random_string(6), imagedata)
     is_image_file = check_if_image_file(raw_image_file_path)
-    logger.info(raw_image_name)
-    logger.info(is_image_file)
     if is_image_file:
         img = Image.open(raw_image_file_path)
         raw_image_size = img.size
-        if img.mode == 'RGBA':
-            img.load()
-            background = Image.new("RGB", raw_image_size, (255, 255, 255))
-            background.paste(img, mask=img.split()[3])
-            background.save(raw_image_file_path, 'JPEG', quality=50)
-        rebuild_image_and_remove_exif(raw_image_file_path)
-        converted_pdf_path = convert_image_to_pdf(raw_image_file_path)
-        converted_image_from_pdf = convert_pdf_to_image(
-            converted_pdf_path, raw_image_size)
-        if converted_image_from_pdf:
-            os.remove(converted_pdf_path)
-            os.remove(raw_image_file_path)
-            img_io = BytesIO()
-            converted_image_from_pdf.save(img_io, 'JPEG', quality=50)
-            return ContentFile(
-                img_io.getvalue(),
-                '%s.jpg' % raw_image_name if raw_image_name else get_random_string(24))
-            # return rebuild_image_and_remove_exif(converted_image_from_pdf, raw_image_name)
+        if raw_image_size[0] <= 2000:
+            if img.mode == 'RGBA':
+                img.load()
+                background = Image.new("RGB", raw_image_size, (255, 255, 255))
+                background.paste(img, mask=img.split()[3])
+                background.save(raw_image_file_path, 'JPEG', quality=80)
+            rebuild_image_and_remove_exif(raw_image_file_path)
+            converted_pdf_path = convert_image_to_pdf(raw_image_file_path)
+            converted_image_from_pdf = convert_pdf_to_image(
+                converted_pdf_path, raw_image_size)
+            if converted_image_from_pdf:
+                os.remove(converted_pdf_path)
+                os.remove(raw_image_file_path)
+                img_io = BytesIO()
+                converted_image_from_pdf.save(img_io, 'JPEG', quality=80)
+                return ContentFile(
+                    img_io.getvalue(),
+                    '%s.jpg' % raw_image_name if raw_image_name else get_random_string(24))
+                # return rebuild_image_and_remove_exif(converted_image_from_pdf, raw_image_name)
     os.remove(raw_image_file_path)
