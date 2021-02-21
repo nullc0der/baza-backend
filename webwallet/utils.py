71136859133fd26125da6a5cc04d7ee113e93e97
prasanta <prasantakakati1994@gmail.com>
2021-02-22 02:56:21 +0530

    small changes

--- webwallet/utils.py
+++ webwallet/utils.py
@@ -8,15 +8,17 @@ def send_main_wallet_low_balance_email_to_admins() -> bool:
         'webwallet/main_wallet_low_balance.html')
     server = 'Live' if settings.SITE_TYPE == 'production' else 'Beta'
     wallet_address = settings.PROXC_TO_REAL_FROM_ADDRESS
-    msg = EmailMultiAlternatives(
-        subject='%s Server Main Wallet Balance Is Low' % server,
-        body='Hi\n, The main wallet of %s server is low,\n'
-        ' please send fund to this address %s' % (server, wallet_address),
-        from_email='system@baza.foundation',
-        to=settings.SITE_OWNER_EMAILS
-    )
-    msg.attach_alternative(email_template.render({
-        'server': server,
-        'wallet_address': wallet_address
-    }), "text/html")
-    msg.send()
+    for email_id in settings.SITE_OWNER_EMAILS:
+        msg = EmailMultiAlternatives(
+            subject='%s Server Main Wallet Balance Is Low' % server,
+            body='Hi\n, The main wallet of %s server is low,\n'
+            ' please send fund to this address %s' % (
+                server.lower(), wallet_address),
+            from_email='system@baza.foundation',
+            to=[email_id]
+        )
+        msg.attach_alternative(email_template.render({
+            'server': server,
+            'wallet_address': wallet_address
+        }), "text/html")
+        msg.send()
