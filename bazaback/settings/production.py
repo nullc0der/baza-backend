10f15cb17c950cbcfcf3aa498a188ad4e885191c
prasanta <prasantakakati1994@gmail.com>
2023-01-08 19:44:33 +0530

    testing some logging

--- bazaback/settings/production.py
+++ bazaback/settings/production.py
@@ -71,16 +71,24 @@ sentry_sdk.init(
 # Logging
 
 LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "({asctime}) {levelname}: {name}.{message}",
+            "style": "{",
         },
     },
-    'root': {
-        'handlers': ['console'],
-        'level': 'INFO',
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "verbose"
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "DEBUG",
     },
 }
 
